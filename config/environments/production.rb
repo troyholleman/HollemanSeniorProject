b35202ef026df71f789fad911b5e56e595aaa720
troyholleman <troyholleman@gmail.com>
2014-12-01 12:55:18 -0500

    Heroku Attempt 4
    - Imported d3.js
    - HOPEFULLY fixed Google Fonts
    - MAYBE fixed popovers not appearing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # ------------ CUSTOM ------------ #
+  
+  config.assets.precompile += %w( popover.js )
+  
 end
